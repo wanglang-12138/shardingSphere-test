425ff798dcb7ae75392c0f2df30468ae07897151
wanglang-12138 <332396560@qq.com>
2022-11-29 10:38:39 +0800

    bug修复

--- src/main/java/com/wl/shardingsphere/domain/Order.java
+++ src/main/java/com/wl/shardingsphere/domain/Order.java
@@ -4,6 +4,9 @@ import lombok.*;
 
 import javax.persistence.*;
 
+/**
+ * @author WangLang
+ */
 @Data
 @Getter
 @Setter
@@ -16,12 +19,13 @@ public class Order {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "order_id")
-    private Long orderId;
+    private Long order_id;
 
     @Column(name = "order_name")
     private String orderName;
 
     @ManyToOne
+    @JoinColumn(name = "user_id")
     private User user;
 
 }
--- src/main/java/com/wl/shardingsphere/domain/User.java
+++ src/main/java/com/wl/shardingsphere/domain/User.java
@@ -4,6 +4,9 @@ import lombok.*;
 
 import javax.persistence.*;
 
+/**
+ * @author WangLang
+ */
 @Data
 @Getter
 @Setter
